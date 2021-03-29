dddcde63b34955ad9f79970e383ead50ef03dd8a
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-03-29 12:50:11 +0100

    Add Todo functionnality using Redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import { v4 as uuidv4 } from "uuid";
 
 const Header = (props) => {
-  console.log(props);
+  const [title, setTitle] = useState("");
+
+  //handle Title Input Value Change
+  const handleChange = (event) => setTitle(event.target.value);
+
+  //Handle Add Todo Button CLick
+  const handleClick = () => {
+    if (title) {
+      props.actions.addTodo({ id: uuidv4(), title: title, completed: false });
+    } else {
+      alert("Please enter the todo title");
+    }
+  };
+
   return (
     <header className="App__header">
       <h1 className="App__title">Todo App</h1>
 
       <div className="App__input-container">
-        <input type="text" className="App__input" placeholder="Title..." />
-        <button className="App__add-btn">Add</button>
+        <input
+          type="text"
+          className="App__input"
+          placeholder="Title..."
+          onChange={handleChange}
+        />
+        <button className="App__add-btn" onClick={handleClick}>
+          Add
+        </button>
       </div>
     </header>
   );
--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,18 +1,18 @@
 import { ADD_TODO, DELETE_TODO, SET_COMPLETED } from "../constants/constants";
-import { v4 as uuidv4 } from "uuid";
 
 const initialState = {
-  todos: [
-    { id: uuidv4(), title: "Eate..", completed: true },
-    { id: uuidv4(), title: "Coding..", completed: true },
-    { id: uuidv4(), title: "Training..", completed: false },
-  ],
+  todos: [],
 };
 
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_TODO:
-      return state;
+    case ADD_TODO: {
+      return {
+        ...state,
+        todos: [...state.todos, action.todo],
+      };
+    }
+
     case DELETE_TODO:
       return state;
     case SET_COMPLETED:
